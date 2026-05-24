48721a6df22a1833867bd6be0bb3cfca4e25c5a0
Sabrina-Toledo <sabrina.toledo@sptech.school>
2026-05-23 23:57:19 -0300

    css atualizado

--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -3,16 +3,16 @@ var database = require("../database/config");
 function buscarUltimasMedidas(idUsuario) {
 
     var instrucaoSql = `
-    SELECT 
+SELECT 
     respostaQuiz.alternativa AS resposta
-    FROM respostaQuiz 
-    JOIN historicoQuiz ON respostaQuiz.fk_historico = historicoQuiz.idhistorico
-    WHERE historicoQuiz.fk_idusuario = ${idUsuario} AND historicoQuiz.idhistorico = (
-              SELECT MAX(idhistorico) 
-              FROM historicoQuiz 
-              WHERE fk_idusuario = ${idUsuario}
-          );
-    `;
+FROM historicoQuiz
+JOIN respostaQuiz ON respostaQuiz.fk_historico = historicoQuiz.idhistorico
+LEFT JOIN historicoQuiz as historicoMaisRecente
+ON historicoMaisRecente.fk_idusuario = historicoQuiz.fk_idusuario
+AND historicoMaisRecente.idhistorico > historicoQuiz.idhistorico
+WHERE historicoQuiz.fk_idusuario = ${idUsuario}
+AND historicoMaisRecente.idhistorico IS NULL;
+`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
